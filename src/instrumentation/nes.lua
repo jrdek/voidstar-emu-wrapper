f0f5dcbfaad77c8a7c88d1b6cdee54261fd9ee95
Joey Rudek <rudek.joseph@gmail.com>
2026-05-07 00:10:56 -0400

    (untested) fix coverage offsets

--- src/instrumentation/nes.lua
+++ src/instrumentation/nes.lua
@@ -12,11 +12,19 @@ local module = {};
 local run_symtbl = nil;
 local run_symtblpath = nil;
 local run_moduleoffset = nil;
+local run_addridxs = nil;
+local run_guardcount = nil;
 
+
+local function full_addr_from(bank, addr)
+    return (bank * 0x10000) + addr;
+end
 
 function module.declare_guard(symtbl, game_name, addr, bank)
     bank = bank or 0;
-    local full_addr = (bank * 0x10000) + addr;
+    local full_addr = full_addr_from(bank, addr);
+    run_guardcount = run_guardcount + 1;
+    run_addridxs[full_addr] = run_guardcount;
     return symtbl:write_position(
         game_name,
         "",
@@ -34,12 +42,12 @@ local function init_symbols(code_chunks, game_name)
     run_symtblpath = "/opt/luasrc/" .. game_name .. ".tsv";
     --run_symtblpath = "/tmp/" .. game_name .. ".tsv";
     run_symtbl = assert(symbol_table.create(run_symtblpath, game_name, "asm6502"));
-    local guard_count = 0;
+    run_guardcount = 0;
+    run_addridxs = {};
     for addr, _ in pairs(code_chunks) do
         module.declare_guard(run_symtbl, game_name, addr);
-        guard_count = guard_count + 1;
     end
-    return guard_count;
+    return run_guardcount;
 end
 
 function module.init_coverage(code_chunks, game_name)
@@ -47,8 +55,10 @@ function module.init_coverage(code_chunks, game_name)
     run_moduleoffset = voidstar.initCoverageModule(num_guards, run_symtblpath);
 end
 
-function module.notify(addr)
-    local loc = run_moduleoffset + addr;
+function module.notify(addr, bank)
+    assert(run_addridxs);  -- this fails if notify() is called before init_symbols()!
+    bank = bank or 0;
+    local loc = run_moduleoffset + run_addridxs[full_addr_from(bank, addr)];
     return voidstar.notifyCoverage(loc);
 end
 
